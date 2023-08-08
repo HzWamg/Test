aba19311ae898af7f45a173947ea3cced8bb6682
HzWamg <wamghongzhe@gmail.com>
2023-08-08 17:02:19 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// * @Author: HzWamg 
+//  * @Date: 2023-08-08 11:52:14
+//  * @Description: 
+//  * Copyright (c) 2023 by HzWamg, All Rights Reserved.
+fn main() {
+        println!("Hello, world!");
+}
